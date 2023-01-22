7c7578814a47b65fb819d385412def88344d279c
Valihan <valihanburhanov@yandex.ru>
2023-01-22 14:36:26 +0300

    Task2 (complete)

--- c#hw3.cs
+++ c#hw3.cs
@@ -1,17 +1,22 @@
-﻿// Задача 19
-// Напишите программу, которая принимает на вход 
-// пятизначное число и проверяет, является ли оно палиндромом.
-// 14212 -> нет
-// 12821 -> да
-// 23432 -> да
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух 
+// точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.Write("Введите пятизначное число: ");
-int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
 
-if ((N/10000 == N%10) && (N/1000%10 == N/10%10)){
-    Console.WriteLine($"{N} -> да");
-}
-else
-{
-    Console.WriteLine($"{N} -> нет");
-}
+double distance3D = Math.Sqrt((Math.Pow((x2 - x1), 2)) + (Math.Pow((y2 - y1), 2)) + (Math.Pow((z2 - z1), 2)));
+
+Console.WriteLine($"{Math.Round(distance3D, 2)}");
