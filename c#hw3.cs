3ec8a711f1aa7b2442c6b656c01153a5dbce0abd
Valihan <valihanburhanov@yandex.ru>
2023-01-22 14:40:55 +0300

    task3 (complete)

--- c#hw3.cs
+++ c#hw3.cs
@@ -1,22 +1,22 @@
-﻿// Задача 21
-// Напишите программу, которая принимает на вход координаты двух 
-// точек и находит расстояние между ними в 3D пространстве.
-// A (3,6,8); B (2,1,-7), -> 15.84
-// A (7,-5, 0); B (1,-1,9) -> 11.53
+﻿// Задача 23
 
-Console.Write("Введите x1: ");
-int x1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите y1: ");
-int y1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите z1: ");
-int z1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите x2: ");
-int x2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите y2: ");
-int y2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите z2: ");
-int z2 = Convert.ToInt32(Console.ReadLine());
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
-double distance3D = Math.Sqrt((Math.Pow((x2 - x1), 2)) + (Math.Pow((y2 - y1), 2)) + (Math.Pow((z2 - z1), 2)));
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
 
-Console.WriteLine($"{Math.Round(distance3D, 2)}");
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{N} -> ");
+for (int i = 1; i <= N; i++)
+{
+    if (i != N)
+    {
+    Console.Write($"{Math.Pow(i,3)}, ");
+    }
+    else 
+    {
+    Console.Write($"{Math.Pow(i,3)}. "); 
+    }
+}
